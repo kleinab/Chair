b1edaaa7b1db163462668e9858110a224edfd147
Abby Klein <abby@zearn.org>
2016-12-15 23:55:33 -0500

    move data to server side

--- client/App.js
+++ client/App.js
@@ -1,20 +1,3 @@
-const users = [
-  {id: 1, name: 'Abby'},
-  {id: 2, name: 'Tyler'},
-];
-
-const questions = [
-  {id: 1, question: 'When is the last time you cried?'},
-  {id: 2, question: 'What is your relationship with your parents?'},
-  {id: 3, question: 'When is the last time you felt proud of yourself?'},
-];
-
-const Data = {
-  users,
-  questions,
-};
-
-
 class Header extends React.Component {
   render() {
     return (
